83c5ec6c180c4f706f6513e3ce9311b9cb482d89
Derek Fong <i.am@derekfong.me>
2016-10-18 22:13:59 +0800

    style: format code

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,11 @@
-// this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
+import { platformNativeScriptDynamic, NativeScriptModule } from 'nativescript-angular/platform';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    imports: [NativeScriptModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [NativeScriptModule],
 })
 class AppComponentModule {}
 
