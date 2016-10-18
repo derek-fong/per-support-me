bd94b93b76e4239ddb17ac1b3a62e5ed79a653d4
Derek Fong <i.am@derekfong.me>
2016-10-18 20:39:10 +0800

    chore: remove legacy code

--- src/app/index.constants.js
+++
@@ -1,9 +0,0 @@
-/* global malarkey:false, moment:false */
-(function() {
-  'use strict';
-
-  angular
-    .module('healthhack')
-    .constant('malarkey', malarkey)
-    .constant('moment', moment);
-})();
