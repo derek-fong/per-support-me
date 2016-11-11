ce0cf67d9cada86a7bdbf2456cff8348411efa16
Derek Fong <i.am@derekfong.me>
2016-11-11 20:02:54 +0800

    fix(stories): show all stories on load

--- src/app/core/shared/filter.service.ts
+++ src/app/core/shared/filter.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/Rx';
 
 @Injectable()
 export class FilterService {
-  private contentType = new Subject<string>();
-  private keyword = new Subject<string>();
+  private contentType = new BehaviorSubject<string>(undefined);
+  private keyword = new BehaviorSubject<string>(undefined);
 
   contentType$ = this.contentType.asObservable();
   keyword$ = this.keyword.asObservable();
--- src/app/core/side-nav/side-nav.component.ts
+++ src/app/core/side-nav/side-nav.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy
+} from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
 
 import { FilterService } from '../shared/filter.service';
 
@@ -7,17 +12,24 @@ import { FilterService } from '../shared/filter.service';
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
-export class SideNavComponent implements OnInit {
+export class SideNavComponent implements OnInit, OnDestroy {
+  private contentTypeSub: Subscription;
   private elementID: string = 'side-nav-left';
   private keyword: string;
 
   constructor(private filterService: FilterService) { }
 
   ngOnInit() {
-    this.filterService.contentType$.subscribe((res: string) => {
+    this.contentTypeSub = this.filterService.contentType$.subscribe((res: string) => {
       console.log(res);
     });
     this.filterService.setKeyword(this.keyword);
+  }
+
+  ngOnDestroy(): void {
+    if (this.contentTypeSub) {
+      this.contentTypeSub.unsubscribe();
+    }
   }
 
   onKeywordChange(keyword: string) {
